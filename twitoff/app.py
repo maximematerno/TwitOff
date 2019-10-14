d21510cc7d2cac7faa033a32ce41859907dc6735
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2019-10-14 02:04:59 -0700

    model.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from .model import DB
 
 def create_app():
     """Create and configure an instance of the flask application"""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db.sqlite3'
+    DB.init_app(app)
 
     @app.route('/')
     def root():

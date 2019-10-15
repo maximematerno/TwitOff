0ef675840689d910836ed6a3741709fd64f717b0
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2019-10-14 17:16:18 -0700

    Assignment

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-from .model import DB
+from decouple import config
+from dotenv import load_dotenv
+from flask import Flask, render_template
+from .model import DB, User
 
 def create_app():
     """Create and configure an instance of the flask application"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/maximevacher-materno/Desktop/TwitOff/db.sqlite3' 
     DB.init_app(app)
 
     @app.route('/')

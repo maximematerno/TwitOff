5a47d5f756bc0a6c7d35ce133223d6e4c35ade49
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2019-10-16 00:47:17 -0700

    Assignment

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,33 +1,78 @@
+from flask import Flask, render_template, request
 from decouple import config
 from dotenv import load_dotenv
-from flask import Flask, render_template
 from .model import DB, User
+from .predict import predict_user
+from .twitter import add_or_update_user, update_all_users, add_default_users
 
 load_dotenv()
 
+
 def create_app():
-    """Create and configure an instance of the flask application"""
-    app = Flask(__name__,template_folder='template')
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL') 
+    """Create and configure an instance of the Flask application"""
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
-
-    def add_user(username):
-        #get twitter user data from tweepy API
-        twitter_user = TWITTER.get_user(username)
-
-        # Add user info to User table in database
-        db_user = User(id=twitter_user.id, name=twitter_user.screen_name)
-        DB.session.add(db_user)
 
     @app.route('/')
     def root():
-        return render_template('home.html', users= User.query.all())
+        return render_template('base.html',
+                               title='Home',
+                               users=User.query.all())
 
-    @app.route("/about/")
-    def about():
-        return render_template('about.html')
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html',
+                               title=name,
+                               tweets=tweets,
+                               message=message)
 
-    if __name__=="__main__":
-        app.run(debug=True)    
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, tweet_text)
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html',
+                               title='Prediction',
+                               message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
+    @app.route('/update')
+    def update():
+        update_all_users()
+        return render_template('base.html',
+                               users=User.query.all(),
+                               title='All Tweets updated!')
+
+    @app.route('/add_default')
+    def add_default():
+        add_default_users()
+        return render_template('base.html',
+                               users=User.query.all(),
+                               title='Added Default Users to Database!')
 
     return app
--- twitoff/model.py
+++ twitoff/model.py
@@ -1,14 +1,24 @@
-"""SQLALchemy models for TwitOFF"""
+"""SQLAlchemy models for TwitOff"""
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
+
 class User(DB.Model):
     """Twitter users that we query and store historical tweets"""
-    id = DB.Column(DB.Integer, primary_key =True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     username = DB.Column(DB.String(15), unique=True, nullable=False)
+    followers = DB.Column(DB.BigInteger, nullable=False)
+    # Tweet IDs are ordinal ints, so we can fetch the most recent tweets.
+    newest_tweet_id = DB.Column(DB.BigInteger, nullable=False)
 
-class Tweets(DB.Model):
+
+class Tweet(DB.Model):
     """Stores tweets"""
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))   
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger,
+                        DB.ForeignKey('user.id'),
+                        nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))   
